eded3a7d0e05d9d79a7728d4732244db2d74a819
Daniel Wayota <danielglez1990@gmail.com>
2019-06-05 19:46:51 +0100

    Project initialization

---
+++ Cerebro/Network.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cerebro
+{
+    public class Network
+    {
+    }
+}
---
+++ Examples/Runner.cs
@@ -0,0 +1,15 @@
+﻿using Cerebro;
+using System;
+
+namespace Examples
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            Network net = new Network();
+
+            Console.WriteLine("Hello network");
+        }
+    }
+}
